8310e569a81554bae1b9eb80d2e6f47301a10bac
chinchay <35080984+chinchay@users.noreply.github.com>
2024-10-18 12:51:41 -0300

    style: Add an image and rearrange displaying

--- MegaDesk1/MainMenu.Designer.cs
+++ MegaDesk1/MainMenu.Designer.cs
@@ -30,11 +30,13 @@
         {
             this.Exit = new System.Windows.Forms.Button();
             this.AddQuote = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // Exit
             // 
-            this.Exit.Location = new System.Drawing.Point(713, 415);
+            this.Exit.Location = new System.Drawing.Point(254, 205);
             this.Exit.Name = "Exit";
             this.Exit.Size = new System.Drawing.Size(75, 23);
             this.Exit.TabIndex = 0;
@@ -44,23 +46,34 @@
             // 
             // AddQuote
             // 
-            this.AddQuote.Location = new System.Drawing.Point(344, 51);
+            this.AddQuote.Location = new System.Drawing.Point(12, 12);
             this.AddQuote.Name = "AddQuote";
-            this.AddQuote.Size = new System.Drawing.Size(75, 23);
+            this.AddQuote.Size = new System.Drawing.Size(116, 23);
             this.AddQuote.TabIndex = 1;
             this.AddQuote.Text = "Add Quote";
             this.AddQuote.UseVisualStyleBackColor = true;
             this.AddQuote.Click += new System.EventHandler(this.AddQuote_Click);
             // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::MegaDesk1.Properties.Resources.desk;
+            this.pictureBox1.Location = new System.Drawing.Point(156, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(173, 187);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
             // MainMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(341, 240);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.AddQuote);
             this.Controls.Add(this.Exit);
             this.Name = "MainMenu";
             this.Text = "Main Menu";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -69,6 +82,7 @@
 
         private System.Windows.Forms.Button Exit;
         private System.Windows.Forms.Button AddQuote;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
